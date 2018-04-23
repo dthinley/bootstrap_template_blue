74f5eb5c40c7a4c947ed4a13eeb223d381ede5b1
Dorjee Thinlay <dthinley@yahoo.com>
2018-04-23 11:58:33 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,17 +2,12 @@ $(document).ready(function(){
 	$('.category_items').click(function(){
 		var category =$(this).attr('id');
 		if(category=='all'){
-			$('.project_list').addClass('hide');
-			setTimeout(function(){
-				$('.project_list').removeClass('hide');
-
-			}, 300);
-
-		} else{
-			$('.project_list').addClass('hide');
-			setTimeout(function(){
-				$('.' + category).removeClass('hide')
-			}, 300);
+			$(".project_list").show("1000");
+		}else
+		{
+			$(".project_list").not("."+category).hide("1000");
+			$(".project_list").filter("."+category).show("1000");
+			
 		}
 
 		
